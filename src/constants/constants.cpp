ff76272d7e37b0e59796749727e3cd66c1d4891e
Fernando Ramirez <ramirez.fernando2003@gmail.com>
2025-11-20 00:23:49 -0800

    chore: update window size comment

--- src/constants/constants.cpp
+++ src/constants/constants.cpp
@@ -3,7 +3,7 @@
 namespace Constants {
 const int FRAME_DELAY =
     1000 / 30;  // The number of milliseconds in a second divided by FPS
-const int WINDOW_SIZE = 768;    // The size of the GUI window (736 x 736)
+const int WINDOW_SIZE = 768;    // The size of the GUI window (768 x 768)
 const int CHARACTER_SIZE = 64;  // The size of the character (64 x 64)
 const int CHARACTER_MOVEMENT_GAP =
     20;  // The number of pixels to move the character
