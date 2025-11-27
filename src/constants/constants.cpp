95aab1a4e1a4daa9ab6ede82f50bc21d9fb371ef
Fernando Ramirez <ramirez.fernando2003@gmail.com>
2025-11-27 00:20:10 -0800

    chore: remove unused assets

--- src/constants/constants.cpp
+++ src/constants/constants.cpp
@@ -3,15 +3,9 @@
 namespace Constants {
 const int FRAME_DELAY =
     1000 / 20;  // The number of milliseconds in a second divided by FPS
-const int WINDOW_SIZE = 768;    // The size of the GUI window (768 x 768)
-const int CHARACTER_SIZE = 64;  // The size of the character (64 x 64)
+const int WINDOW_SIZE = 768;  // The size of the GUI window (768 x 768)
 const int CHARACTER_MOVEMENT_GAP =
     20;  // The number of pixels to move the character
-const char* CHARACTER_ELF_DOWN_FOLDER_PATH = "assets/characters/elf/down/";
-const char* CHARACTER_ELF_LEFT_FOLDER_PATH = "assets/characters/elf/left/";
-const char* CHARACTER_ELF_RIGHT_FOLDER_PATH = "assets/characters/elf/right/";
-const char* CHARACTER_ELF_UP_FOLDER_PATH = "assets/characters/elf/up/";
-
 const char* TILE_SET_PATH = "assets/sprites/map/tile_set.png";
 const int TILE_WIDTH = 16;
 const int TILE_HEIGHT = 16;
--- src/constants/constants.h
+++ src/constants/constants.h
@@ -4,12 +4,7 @@ namespace Constants {
 
 extern const int FRAME_DELAY;
 extern const int WINDOW_SIZE;
-extern const int CHARACTER_SIZE;
 extern const int CHARACTER_MOVEMENT_GAP;
-extern const char* CHARACTER_ELF_DOWN_FOLDER_PATH;
-extern const char* CHARACTER_ELF_LEFT_FOLDER_PATH;
-extern const char* CHARACTER_ELF_RIGHT_FOLDER_PATH;
-extern const char* CHARACTER_ELF_UP_FOLDER_PATH;
 extern const char* TILE_SET_PATH;
 extern const int TILE_WIDTH;
 extern const int TILE_HEIGHT;
