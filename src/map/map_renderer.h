c03be74203d68beaf7d790d9abe3fc8950dbae43
Fernando Ramirez <ramirez.fernando2003@gmail.com>
2025-12-04 01:24:54 -0600

    chore: add MapRenderer class comment

--- src/map/map_renderer.h
+++ src/map/map_renderer.h
@@ -6,7 +6,7 @@
 
 // This class manages rendering of the game map using various tile sets. The map
 // consists of multiple layers such as grass, water, buildings, etc. The map is
-// 16x16 tiles, each tile is 48x48 pixels.
+// 16x16 tiles, each tile is 48x48 pixels (scaled up by 3 from 16x16).
 class MapRenderer {
  public:
   MapRenderer();
