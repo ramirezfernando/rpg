1edd995cccf8781c56babe1fe9894622adbfecc7
Fernando Ramirez <ramirez.fernando2003@gmail.com>
2026-02-02 01:28:26 -0800

    refactor: remove unused member functions

--- src/entities/actions/npc_movement_handler.h
+++ src/entities/actions/npc_movement_handler.h
@@ -11,11 +11,4 @@ class NpcMovementHandler {
  private:
   // Generate a random direction for the NPC to face.
   static Direction GetRandomDirection();
-
-  // Attempt to move the NPC in the given direction.
-  // Returns true if movement was successful, false if blocked or invalid.
-  static bool TryMove(Entity* npc, Map* map, Direction direction);
-
-  // Check if movement is valid (not out of bounds, no collision).
-  static bool IsMovementValid(int x, int y, Map* map);
 };
