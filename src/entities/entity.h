074b470b0d7b76e05f787164d8224861e6fbd737
Fernando Ramirez <ramirez.fernando2003@gmail.com>
2026-02-07 22:25:42 -0800

    chore: add comment about non-owning ptr

--- src/entities/entity.h
+++ src/entities/entity.h
@@ -39,8 +39,7 @@ class Entity {
   void SetSprite(Sprite* sprite) { sprite_ = sprite; }
 
  private:
-  // TODO(fernandoramirez): Clean up raw pointer or check if I can use unique
-  // pointer.
+  // Non-owning pointer to cached sprite.
   Sprite* sprite_;
   Direction direction_{Direction::Down};
   Action action_{Action::Idle};
