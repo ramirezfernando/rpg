e8ce49e53a845ca6cb313df81df90a17de9ba608
Fernando Ramirez <ramirez.fernando2003@gmail.com>
2026-02-20 23:03:26 -0800

    feat: add client class

--- src/network/client.cpp
+++ src/network/client.cpp
@@ -1,1 +1,61 @@
 #include "client.h"
+
+#include <netdb.h>
+#include <sys/_types/_ssize_t.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include <cerrno>
+#include <cstring>
+#include <memory>
+#include <string>
+
+#include "network/socket.h"
+#include "util/logger.h"
+
+// The typical client pattern is either connect → send/recv
+std::unique_ptr<Client> Client::Create(const std::string& host,
+                                       const std::string& port) {
+  const addrinfo* address_info = Socket::GetAddressInfo(host, port, false);
+  if (address_info == nullptr) {
+    return nullptr;
+  }
+
+  const int socket_file_descriptor =
+      socket(address_info->ai_family, address_info->ai_socktype,
+             address_info->ai_protocol);
+  if (socket_file_descriptor == -1) {
+    Logger::Error("Client", strerror(errno));
+    return nullptr;
+  }
+
+  const int status = connect(socket_file_descriptor, address_info->ai_addr,
+                             address_info->ai_addrlen);
+  // fcntl(fd, F_SETFL, O_NONBLOCK);
+  if (status == -1) {
+    Logger::Error("Client", strerror(errno));
+    close(socket_file_descriptor);
+    return nullptr;
+  }
+
+  Logger::Debug("Client", "Created");
+  return std::unique_ptr<Client>(new Client(socket_file_descriptor));
+}
+
+ssize_t Client::Send(const void* buf, size_t len) {
+  const ssize_t bytes_sent = send(GetFileDescriptor(), buf, len, 0);
+  if (bytes_sent == -1) {
+    Logger::Error("Client", strerror(errno));
+  }
+  Logger::Debug("Client", "Send");
+  return bytes_sent;
+}
+
+ssize_t Client::Receive(void* buf, size_t len) {
+  const ssize_t bytes_read_into_buffer = recv(GetFileDescriptor(), buf, len, 0);
+  if (bytes_read_into_buffer == -1) {
+    Logger::Error("Client", strerror(errno));
+  }
+  Logger::Debug("Client", "Receive");
+  return bytes_read_into_buffer;
+}
--- src/network/client.h
+++ src/network/client.h
@@ -1,9 +1,21 @@
-class Client {
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#include "socket.h"
+
+class Client : public Socket {
  public:
-  Client() = default;
   ~Client() = default;
   Client(const Client& other) = delete;
   Client operator=(const Client& other) = delete;
   Client(Client&& other) = delete;
   Client operator=(Client&& other) = delete;
+
+  static std::unique_ptr<Client> Create(const std::string& host,
+                                        const std::string& port);
+  ssize_t Send(const void* buf, size_t len);
+  ssize_t Receive(void* buf, size_t len);
+
+ private:
+  Client(int socket_file_descriptor) : Socket(socket_file_descriptor) {}
 };
