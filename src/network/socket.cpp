6328892cae4a00c156d920ec51c6e46112dd4fc4
Fernando Ramirez <ramirez.fernando2003@gmail.com>
2026-02-20 23:02:41 -0800

    feat: add socket class

--- src/network/socket.cpp
+++ src/network/socket.cpp
@@ -1,1 +1,41 @@
 #include "socket.h"
+
+#include <netdb.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include <cerrno>
+#include <cstring>
+#include <string>
+
+#include "util/logger.h"
+
+Socket::Socket(int socket_file_descriptor)
+    : socket_file_descriptor_{socket_file_descriptor} {}
+
+Socket::~Socket() {
+  close(socket_file_descriptor_);
+}
+
+struct addrinfo* Socket::GetAddressInfo(const std::string& host,
+                                        const std::string& port, bool passive) {
+  struct addrinfo hints{};
+  // NOLINTNEXTLINE(misc-const-correctness)
+  struct addrinfo* result{nullptr};
+
+  memset(&hints, 0, sizeof hints);  // Make sure the struct is empty.
+  hints.ai_family = AF_UNSPEC;      // Don't care IPv4 or IPv6.
+  hints.ai_socktype = SOCK_DGRAM;   // UDP socket.
+  if (passive) {
+    hints.ai_flags = AI_PASSIVE;  // Fill in my IP for me.
+  }
+
+  const char* node = host.empty() ? nullptr : host.c_str();
+  // NOLINTNEXTLINE(misc-const-correctness)
+  int status = getaddrinfo(node, port.c_str(), &hints, &result);
+  if (status != 0) {
+    Logger::Error("Socket", gai_strerror(errno));
+  }
+  // TODO(ramirezfernando): Be sure to free this pointer.
+  return result;
+}
--- src/network/socket.h
+++ src/network/socket.h
@@ -1,9 +1,24 @@
+#pragma once
+
+#include <sys/types.h>
+
+#include <cstddef>
+#include <memory>
+#include <string>
+
 class Socket {
  public:
-  Socket() = default;
-  ~Socket() = default;
+  Socket(int socket_file_descriptor);
+  ~Socket();
   Socket(const Socket& other) = delete;
-  Socket operator=(const Socket& other) = delete;
+  Socket& operator=(const Socket& other) = delete;
   Socket(Socket&& other) = delete;
-  Socket operator=(Socket&& other) = delete;
+  Socket& operator=(Socket&& other) = delete;
+
+  static struct addrinfo* GetAddressInfo(const std::string& host,
+                                         const std::string& port, bool passive);
+  int GetFileDescriptor() const { return socket_file_descriptor_; }
+
+ private:
+  int socket_file_descriptor_;
 };
