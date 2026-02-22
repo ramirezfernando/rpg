740796294f740d12792001f52c4685680b7cb7c4
Fernando Ramirez <ramirez.fernando2003@gmail.com>
2026-02-21 19:25:22 -0800

    feat: decrease max buffer size

--- src/constants/network_constants.h
+++ src/constants/network_constants.h
@@ -3,6 +3,6 @@
 namespace Constants {
 
 inline constexpr int PORT = 8080;
-inline constexpr int MAX_BUFFER_SIZE = 1024;
+inline constexpr int MAX_BUFFER_SIZE = 16;  // Modify based on needs.
 
 }  // namespace Constants
